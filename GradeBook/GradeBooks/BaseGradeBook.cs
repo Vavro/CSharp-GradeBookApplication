49690baca2286f1d86a67b44b1dc40856f7953ab
Petr Vavro <BlackSun.Petr@gmail.com>
2018-06-27 15:22:24 +0200

    7.2 - getGPA

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -109,18 +109,13 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
-            switch (letterGrade)
+            var valueOfGrade = Math.Abs((int)letterGrade - (int)'E');
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
             {
-                case 'A':
-                    return 4;
-                case 'B':
-                    return 3;
-                case 'C':
-                    return 2;
-                case 'D':
-                    return 1;
+                valueOfGrade++;
             }
-            return 0;
+
+            return valueOfGrade;
         }
 
         public virtual void CalculateStatistics()
