7d2f04cc34a2798fea568b998146be6b4737f492
Petr Vavro <BlackSun.Petr@gmail.com>
2018-06-27 14:25:39 +0200

    3 - ranked grade book ranking

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,22 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires a minimum of 5 students to work");
+            }
+
+            var studentsToDropGrade = this.Students.Count / 5;
+
+            var countOfStudentsWithHigherGrade = this.Students.Count(s => s.AverageGrade > averageGrade);
+
+            var howManyGradesToDrop = countOfStudentsWithHigherGrade / studentsToDropGrade;
+
+            return (char)((int)'A' + howManyGradesToDrop);
         }
     }
 }
